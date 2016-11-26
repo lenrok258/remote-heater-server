2c0a73ca53f9767adf4e1c57b598baea97723340
Kornel Zemla <kornelzemla@gmail.com>
2016-11-26 23:20:24 +0100

    Minor startup enhancements

--- on-git-pull.sh
+++ on-git-pull.sh
@@ -1,4 +1,4 @@
-LOG_FILE=./logs/setup_`date +%Y-%m-%d_%H:%M.log` 
+LOG_FILE=./logs/`date +%Y-%m-%d_%H:%M.log` 
 
 function log {
     echo $1 | tee -a ${LOG_FILE}
@@ -7,6 +7,6 @@ function log {
 log "About to install and restart <<Remote Heater Server>>"
 
 ./install.sh 2>&1 | tee -a ${LOG_FILE}
-./remote-heater-server.sh | tee -a ${LOG_FILE}
+./remote-heater-server.sh & 2>&1 | tee -a ${LOG_FILE}
 
 return 0
--- remote-heater-server.sh
+++ remote-heater-server.sh
@@ -1,3 +1,3 @@
-#!//bin/bash
+#!/bin/bash
 
 node ./remote-heater-server.js
