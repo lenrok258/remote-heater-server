ca00ae0365661c54633719fac06525722c6af155
Kornel Zemla <kornelzemla@gmail.com>
2016-11-26 14:25:16 +0100

    run scripts pt.2

--- on-git-pull.sh
+++ on-git-pull.sh
@@ -1,5 +1,12 @@
-echo "About to install and restart <<Remote Heater Server>>"
+LOG_FILE=./logs/setup_`date +%Y-%m-%d_%H:%M.log`
 
+function log {
+    echo $1 | tee -a ${LOG_FILE}
+}
 
+log "About to install and restart <<Remote Heater Server>>"
+
+./install.sh | tee -a ${LOG_FILE}
+./remote-heater-server.sh | tee -a ${LOG_FILE}
 
 return 0
