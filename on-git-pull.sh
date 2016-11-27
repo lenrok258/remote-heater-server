d4f759e58760cf3b86396ce89562f3c0dcb12976
Kornel Zemla <kornelzemla@gmail.com>
2016-11-27 12:16:17 +0100

    Killing server before startting it again

--- on-git-pull.sh
+++ on-git-pull.sh
@@ -4,6 +4,10 @@ function log {
     echo $1 | tee -a ${LOG_FILE}
 }
 
+log "About to kill <<Remote Heater Server>>"
+kill $(ps aux | grep -v "grep" | grep "remote-heater-server.js" | awk '{print $2}') | tee -a ${LOG_FILE}
+sleep 5
+
 log "About to install and restart <<Remote Heater Server>>"
 
 ./install.sh 2>&1 | tee -a ${LOG_FILE}
