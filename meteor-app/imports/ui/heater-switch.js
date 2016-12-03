720dd5d04051a8c42c701ddf92b6400678defea6
Kornel Zemla <kornelzemla@gmail.com>
2016-12-03 11:24:27 +0100

    Tinkering with meteor pt.2

--- meteor-app/imports/ui/heater-switch.js
+++ meteor-app/imports/ui/heater-switch.js
@@ -1,2 +1,16 @@
 import './heater-switch.html'
 
+Template.heaterSwitch.events({
+
+    'submit #heater-switch-form' (event) {
+        event.preventDefault();
+
+        // const target = event.target;
+        // const requestedTemperature = event.target['target-temp'].value
+
+
+        console.log(event)
+        alert('trolo2')
+    }
+
+})
